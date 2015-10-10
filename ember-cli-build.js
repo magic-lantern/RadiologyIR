d5700ce61285bd57377ba20eaa9376a81c690e56
magic-lantern <magic-lantern@users.noreply.github.com>
2015-10-10 09:14:48 -0600

    Importing UI from prototype, updates to include Bootstrap and remove content security protection

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    'ember-bootstrap': {
+        'importBootstrapCSS': false
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -18,6 +21,9 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
+  // import custom themed bootstrap CSS instead of default from bower_components
+  app.import('vendor/cyborg_bootstrap_theme/bootstrap.css');
 
   return app.toTree();
 };
